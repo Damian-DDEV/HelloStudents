3decf44ae15a446e415b30f3c22b480548d3ba46
Damian <damianmarquesa@gmail.com>
2021-03-16 01:23:10 -0300

    16/03

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -63,73 +63,47 @@ $$(document).on('deviceready', function() {
 });
 
 // Option 1. Using one 'page:init' handler for all pages
-$$(document).on('page:init', function (e) {
-    // Do something here when page loaded and initialized
-    console.log(e);
-
-    //LOGIN
-  $$("#ingresar").on('click', function(){
-    var correod = $$("#correod").val();
-    var claved = $$("#claved").val();
-    firebase.auth().signInWithEmailAndPassword(correod, claved)
-    .then((user) => {
-      emails = correod;
-      mainView.router.navigate('/aulad/');      
-    })
-    .catch((error) => {
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      alert(errorMessage);
-    });
-      });
-      $$("#docente").on('click', function(){
-        mainView.router.navigate('/docente/');
-      });
-      $$("#alumno").on('click', function(){
-        mainView.router.navigate('/alumno/');
-      });
+$$(document).on('page:init', '.page[data-name="index"]', function (e) {
+  $$("#ingresar").on('click', ingresar);
+  if ( $$('#docente').hasClass('azul')) {
+    $$('#docente').removeClass('azul').addClass('rojo');
+    }
+  if ($$('alumno').hasClass('rojo')){
+    $$('#alumno').removeClass('rojo').addClass('azul');
+  }
+  $$("#alumno").on('click', function(){
+    mainView.router.navigate('/alumno/');
   })
-  
+  })
+
 //AULA DOCENTE
 $$(document).on('page:init', '.page[data-name="aulad"]', function (e) {
   db.collection("materia").where("NombreProfesor", "==", emails)
-    .get()
-    .then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-          $$(".aulas").append("<div class='col'><input id='"+doc.id+"' type='button' value='"+doc.data().Nombremateria+"' class='col button button-large button-fill redirn'></input></div>");
-          console.log(doc.id);
-          $$(".redirn").on('click', function(){
-            mainView.router.navigate('/aulan/');
-          })      
-        });
-    })
-    .catch((error) => {
-        console.log("Error getting documents: ", error);
-    });
-  $$("#cerrarsesion").on('click', function(){
-    mainView.router.navigate('/index/');
-    firebase.auth().signOut().then(() => {
-      alert("Se ha cerrado la sesion")
-    }).catch((error) => {
-      alert(error);
-    });
-  })
-  $$("#agregaraula").on('click', function(){
-    var nombreaula = $$("#nombreaula").val();
-    $$(".aulas").append("<div class='col'><input type='button' id='"+doc.id+"' value='"+nombreaula+"' class='col button button-large button-fill'></input></div>");
-    db.collection("materia").doc().set({
-      Nombremateria: ""+nombreaula+"",
-      NombreProfesor: ""+emails+""
-    })
-  .then(() => {
-      console.log("Document successfully written!");
-
+  .get()
+  .then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        $$(".aulas").append("<div class='col'><input type='button' value='"+doc.data().Nombremateria+"' class='col button button-large button-fill redirn espacio'></input></div>");
+        console.log(doc.id);
+        $$(".redirn").on('click', function(){
+          mainView.router.navigate('/aulan/');
+        })      
+      });
   })
   .catch((error) => {
-      console.error("Error writing document: ", error);
+      console.log("Error getting documents: ", error);
   });
+$$("#cerrarsesion").on('click', cerrarsesion);
+$$("#agregaraula").on('click', function(){
+  var nombreaula = $$("#nombreaula").val();
+  $$(".aulas").append("<div class='col'><input type='button' value='"+nombreaula+"' class='col button button-large button-fill redirn espacio'></input></div>");
+  db.collection("materia").doc().set({
+    Nombremateria: ""+nombreaula+"",
+    NombreProfesor: ""+emails+""
   })
-
+.then(() => {
+    console.log("Document successfully written!");
+})
+})
   var bienvenido = db.collection("usuarios").doc(emails);
   bienvenido.get().then(function(doc) {
   if (doc.exists) {
@@ -137,17 +111,91 @@ $$(document).on('page:init', '.page[data-name="aulad"]', function (e) {
     $$("#bienvenido").html(nomb);
 }
 });
-
 })
 
 
 //REGISTRAR
 $$(document).on('page:init', '.page[data-name="registrar"]', function (e) {
-  $$("#registrar").on('click', function(){
-    var correor = $$("#correor").val();
-    var claver = $$("#claver").val();
-    var nombrer = $$("#nombrer").val();
-    var apellidor = $$("#apellidor").val();
+  $$("#registrar").on('click', registrar);
+})
+
+//AULAN
+  $$(document).on('page:init', '.page[data-name="aulan"]', function (e) {
+    db.collection("materia").where("Nombremateria", "==", doc.id)
+    .get().then(function(doc) {
+    if (doc.exists) {
+      console.log(doc);
+    var nombremat = doc.data().Nombremateria;
+   
+  }
+  });
+  })
+
+//ALUMNO
+  $$(document).on('page:init', '.page[data-name="alumno"]', function (e) {
+    if ( $$('#docente').hasClass('rojo') && $$('#alumno').hasClass('azul')) {
+      $$('#docente').removeClass('rojo').addClass('azul');
+      $$('#alumno').removeClass('azul').addClass('rojo');
+      }
+      $$("#docente").on('click', function(){
+        mainView.router.navigate('/docente/');
+      })
+      $$("#ingresar").on('click', ingresar);
+  })
+  
+  $$(document).on('page:init', '.page[data-name="docente"]', function (e) {
+    if ( $$('#docente').hasClass('azul')) {
+      $$('#docente').removeClass('azul').addClass('rojo');
+      }
+    if ($$('#alumno').hasClass('rojo')){
+      $$('#alumno').removeClass('rojo').addClass('azul');
+    }
+    $$("#alumno").on('click', function(){
+      mainView.router.navigate('/alumno/');
+    })
+    $$("#ingresar").on('click', ingresar);
+   })
+
+
+
+
+function ingresar (){
+  var correod = $$("#correod").val();
+  var claved = $$("#claved").val();
+  firebase.auth().signInWithEmailAndPassword(correod, claved)
+  .then((user) => {
+    emails = correod;
+    mainView.router.navigate('/aulad/');      
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    alert(errorMessage);
+  });
+    $$("#docente").on('click', function(){
+      mainView.router.navigate('/docente/');
+    });
+    $$("#alumno").on('click', function(){
+      mainView.router.navigate('/alumno/');
+    });
+}
+
+
+function cerrarsesion(){
+  mainView.router.navigate('/index/');
+  firebase.auth().signOut().then(() => {
+    alert("Se ha cerrado la sesion")
+  }).catch((error) => {
+    alert(error);
+  });
+}
+
+
+function registrar() {
+  var correor = $$("#correor").val();
+  var claver = $$("#claver").val();
+  var nombrer = $$("#nombrer").val();
+  var apellidor = $$("#apellidor").val();
     firebase.auth().createUserWithEmailAndPassword(correor, claver)
     .then((user) => {
       datos = { nombre: nombrer, apellido: apellidor};
@@ -160,28 +208,4 @@ $$(document).on('page:init', '.page[data-name="registrar"]', function (e) {
     var errorMessage = error.message;
     alert(errorMessage);
   });
-  })
-})
-
-//AULAN
-  $$(document).on('page:init', '.page[data-name="aulan"]', function (e) {
-    db.collection("materia").where("Nombremateria", "==", doc.id)
-    .get().then(function(doc) {
-    if (doc.exists) {
-      console.log(doc);
-    var nombremat = doc.data().Nombremateria;
-   
-    
-  }
-  });
-  })
-
-//ALUMNO
-  $$(document).on('page:init', '.page[data-name="alumno"]', function (e) {
-    if ( $$('#docente').hasClass('rojo') ) {
-      $$('#docente').removeClass('rojo').addClass('azul');
-    } else if ($$('#docente').hasClass('rojo')) {
-      $$('#docente').removeClass('rojo').addClass('azul');
-    }
-
-  })
+}
